ff1eadc79f23e7be2bcdd14550b71781bec51a19
bennettbijumathew <bennettbijumathew@gmail.com>
2026-09-07 21:42:06 +1000

    Created page structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+
+<div class="flex flex-col flex-1">
+    <div class="border p-4 flex justify-between">
+        <a href="/"> Bennett Biju Mathew </a>
+
+        <nav class="flex gap-x-2">
+            <a href="/skills"> Skills </a>
+            <a href="/portfolio"> Portfolio </a>
+            <a href="/volunteering"> Volunteering </a>
+            <a href="/work"> Work </a>
+            <a href="/contact"> Contact </a>
+        </nav>
+    </div>
+
+    {@render children()}
+</div>

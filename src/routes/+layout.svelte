842a1c2a568df35bd9145c6249a7f032ac25c98c
bennettbijumathew <bennettbijumathew@gmail.com>
2026-09-11 14:09:13 +1000

    Added adapter to allow project to turn into static pages for Github Pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
     import PageNav from '$lib/components/page-nav.svelte';
+
 	let { children } = $props();
+
+    export const prerender = true;
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-
 
 <div class="flex flex-col flex-1">
     <PageNav/>

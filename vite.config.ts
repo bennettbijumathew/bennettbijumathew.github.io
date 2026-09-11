3c16d0ed2cfb2ffc0b4cd066f217c2ec591490ab
bennettbijumathew <bennettbijumathew@gmail.com>
2026-09-12 00:34:16 +1000

    Added deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from "@tailwindcss/vite";
-import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
@@ -12,13 +11,6 @@ export default defineConfig({
                 runes: ({ filename }) =>
                 filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
             },
-            adapter: adapter({
-                pages: "build",
-                assets: "build",
-                fallback: undefined,
-                precompress: false,
-                strict: true,
-            }),
         }),
     ],
 });

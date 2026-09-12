e3ed7c4f7f9d6aee58ac788b07ed50af7a9588d4
bennettbijumathew <bennettbijumathew@gmail.com>
2026-09-12 11:57:22 +1000

    Reconfigured config files
    Changed svelte.config file to .js file
    
    Added resolve to base link
    
    Added working configuration for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [
         tailwindcss(),
-        sveltekit({
-            // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-            compilerOptions: {
-                runes: ({ filename }) =>
-                filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
-            },
-        }),
+        sveltekit(),
     ],
 });

34e12d66cbffcd8c7faf68daec2238ecba9f9a14
bennettbijumathew <bennettbijumathew@gmail.com>
2026-09-12 11:59:31 +1000

    Changed svelte.config file to .js file

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 
 const dev = process.argv.includes('dev');
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
